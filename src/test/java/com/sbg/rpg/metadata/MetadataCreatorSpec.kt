b3a2f4db71afac314a6527f8a5e042293966e49e
Christian Broomfield <christian.broomfield@posteo.de>
2016-05-19 15:55:19 +0200

    Fix: Ignored tests due to misplaced brace

--- src/test/java/com/sbg/rpg/metadata/MetadataCreatorSpec.kt
+++ src/test/java/com/sbg/rpg/metadata/MetadataCreatorSpec.kt
@@ -51,16 +51,16 @@ class MetadataCreatorSpec: Spek() { init {
             val metadataCreator = YamlMetadataCreator()
 
             it("returns a valid yaml 1.1 string") {
-                val expected = "Frames:" +System.lineSeparator() +
-                               "  - Index: 0" +System.lineSeparator() +
-                               "    Bounds: [0, 0, 50, 50]" +System.lineSeparator()
+                val expected = "Frames:" + System.lineSeparator() +
+                        "  - Index: 0" + System.lineSeparator() +
+                        "    Bounds: [0, 0, 50, 50]" + System.lineSeparator()
 
                 val result = metadataCreator.create(spriteBoundsList)
 
                 assertTrue(result.isNotEmpty(),
-                           "Expected a proper yaml string for single SpriteBounds entry")
+                        "Expected a proper yaml string for single SpriteBounds entry")
                 assertEquals(expected, result,
-                             "Expected $expected, but was $result")
+                        "Expected $expected, but was $result")
             }
         }
 
@@ -73,9 +73,9 @@ class MetadataCreatorSpec: Spek() { init {
                 val result = metadataCreator.create(spriteBoundsList)
 
                 assertTrue(result.isNotEmpty(),
-                           "Expected a proper json string for single SpriteBounds entry")
+                        "Expected a proper json string for single SpriteBounds entry")
                 assertEquals(expected, result,
-                             "Expected $expected, but was $result")
+                        "Expected $expected, but was $result")
             }
         }
 
@@ -88,64 +88,64 @@ class MetadataCreatorSpec: Spek() { init {
                 val result = metadataCreator.create(spriteBoundsList)
 
                 assertTrue(result.isNotEmpty(),
-                           "Expected a plan text string for single SpriteBounds entry")
+                        "Expected a plan text string for single SpriteBounds entry")
+                assertEquals(expected, result,
+                        "Expected $expected, but was $result")
+            }
+        }
+    }
+
+    given("Multiple SpriteBounds") {
+        val spriteBoundsList = listOf(SpriteBounds(0, Rectangle(0, 0, 50, 50)),
+                                      SpriteBounds(1, Rectangle(51, 51, 50, 50)))
+
+        on("converting to yaml") {
+            val metadataCreator = YamlMetadataCreator()
+
+            it("returns a valid yaml 1.1 string") {
+                val expected = "Frames:${System.lineSeparator()}" +
+                "  - Index: 0${System.lineSeparator()}" +
+                "    Bounds: [0, 0, 50, 50]${System.lineSeparator()}" +
+                "  - Index: 1${System.lineSeparator()}" +
+                "    Bounds: [51, 51, 50, 50]${System.lineSeparator()}"
+
+                val result = metadataCreator.create(spriteBoundsList)
+
+                assertTrue(result.isNotEmpty(),
+                        "Expected a valid yaml 1.1 String")
+                assertEquals(expected, result,
+                        "Expected $expected, but was $result")
+            }
+        }
+
+        on("converting to json") {
+            val metadataCreator = JsonMetadataCreator()
+
+            it("returns a valid json string") {
+                val expected = "[{\"frame\":0,\"bounds\":{\"x\":0,\"y\":0,\"width\":50,\"height\":50}},{\"frame\":1,\"bounds\":{\"x\":51,\"y\":51,\"width\":50,\"height\":50}}]"
+
+                val result = metadataCreator.create(spriteBoundsList)
+
+                assertTrue(result.isNotEmpty(),
+                           "Expected a non-empty json string")
                 assertEquals(expected, result,
                              "Expected $expected, but was $result")
             }
         }
 
-        given("Multiple SpriteBounds") {
-            val spriteBoundsList = listOf(SpriteBounds(0, Rectangle(0, 0, 50, 50)),
-                                          SpriteBounds(1, Rectangle(51, 51, 50, 50)))
+        on("converting to text") {
+            val metadataCreator = TextMetadataCreator()
 
-            on("converting to yaml") {
-                val metadataCreator = YamlMetadataCreator()
+            it("returns a plain text string string") {
+                val expected = "0=0 0 50 50" +System.lineSeparator() +
+                               "1=51 51 50 50"
 
-                it("returns a valid yaml 1.1 string") {
-                    val expected = "Frames:" +System.lineSeparator() +
-                    "  - Index: 0" +System.lineSeparator() +
-                    "    Bounds: [0, 0, 50, 50]" +System.lineSeparator() +
-                    "  - Index: 1" +System.lineSeparator() +
-                    "    Bounds: [51, 51, 50, 50]"
+                val result = metadataCreator.create(spriteBoundsList)
 
-                    val result = metadataCreator.create(spriteBoundsList)
-
-                    assertTrue(result.isNotEmpty(),
-                            "Expected a valid yaml 1.1 String")
-                    assertEquals(expected, result,
-                            "Expected $expected, but was $result")
-                }
-            }
-
-            on("converting to json") {
-                val metadataCreator = JsonMetadataCreator()
-
-                it("returns a valid json string") {
-                    val expected = "[{\"frame\":0,\"bounds\":{\"x\":0,\"y\":0,\"width\":50,\"height\":50},{\"frame\":1,\"bounds\":{\"x\":51,\"y\":51,\"width\":50,\"height\":50}}]"
-
-                    val result = metadataCreator.create(spriteBoundsList)
-
-                    assertTrue(result.isNotEmpty(),
-                               "Expected a non-empty json string")
-                    assertEquals(expected, result,
-                                 "Expected $expected, but was $result")
-                }
-            }
-
-            on("converting to text") {
-                val metadataCreator = TextMetadataCreator()
-
-                it("returns a plain text string string") {
-                    val expected = "0=0 0 50 50" +System.lineSeparator() +
-                                   "1=51 51 50 50"
-
-                    val result = metadataCreator.create(spriteBoundsList)
-
-                    assertTrue(result.isNotEmpty(),
-                               "Exepcted a non-empty plain text string")
-                    assertEquals(expected, result,
-                                 "Expected $expected, but was $result")
-                }
+                assertTrue(result.isNotEmpty(),
+                           "Exepcted a non-empty plain text string")
+                assertEquals(expected, result,
+                             "Expected $expected, but was $result")
             }
         }
     }
