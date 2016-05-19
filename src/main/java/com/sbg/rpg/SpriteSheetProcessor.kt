b7dec3c57c255f21d5ef97309c89af0af6600a62
Christian Broomfield <christian.broomfield@posteo.de>
2016-05-19 22:59:41 +0200

    Rem: Unfinished SpriteSheetPacker

--- src/main/java/com/sbg/rpg/SpriteSheetProcessor.kt
+++ src/main/java/com/sbg/rpg/SpriteSheetProcessor.kt
@@ -21,7 +21,6 @@ import com.sbg.rpg.metadata.JsonMetadataCreator
 import com.sbg.rpg.metadata.MetadataCreator
 import com.sbg.rpg.metadata.TextMetadataCreator
 import com.sbg.rpg.metadata.YamlMetadataCreator
-import com.sbg.rpg.packer.SpriteSheetPacker
 import com.sbg.rpg.unpacker.SpriteSheetUnpacker
 import org.apache.logging.log4j.LogManager
 import java.nio.file.Paths
@@ -35,13 +34,15 @@ import kotlin.properties.Delegates
 class SpriteSheetProcessor() {
     private val logger = LogManager.getLogger(SpriteSheetProcessor::class.simpleName)
 
+
+    /*
+     * The MetadataCreator will, in the future, create the Atlas file for each sprite sheet.
+     */
     private var metadataCreator: MetadataCreator by Delegates.notNull()
     private val spriteSheetUnpacker: SpriteSheetUnpacker
-    private val spriteSheetPacker: SpriteSheetPacker
 
     init {
         spriteSheetUnpacker = SpriteSheetUnpacker()
-        spriteSheetPacker = SpriteSheetPacker()
     }
 
     /**
@@ -51,8 +52,6 @@ class SpriteSheetProcessor() {
      *  as well as the output type of the resultant Metadata or Atlas file.
      */
     fun processSpriteSheets(commandLineArguments: CommandLineArguments) {
-        val spriteSheetsWithMetadata = ArrayList<ProcessedSpriteSheet>()
-
         for (rawSpriteSheetPath in commandLineArguments.spriteSheetPaths) {
             logger.debug("Working on $rawSpriteSheetPath")
             val spriteSheetPath = Paths.get(rawSpriteSheetPath)!!.toAbsolutePath()!!
@@ -62,22 +61,10 @@ class SpriteSheetProcessor() {
 
             logger.debug("Writing individual sprites to file in directory ${commandLineArguments.exportFolder}")
             sprites.forEachIndexed { idx, sprite ->
-                ImageIO.write(sprite, "png", Paths.get(commandLineArguments.exportFolder, "sprite_$idx.png").toFile())
+                ImageIO.write(sprite, "png", Paths.get(commandLineArguments.exportFolder, "${spriteSheetPath.fileName}_sprite_$idx.png").toFile())
             }
-
-            logger.trace("Packing sprites")
-            val (packedSpriteSheet, spriteBoundsList) = spriteSheetPacker.packSprites(sprites)
-
-            logger.debug("Creating ${commandLineArguments.metadataOutputFormat} metadata")
-            metadataCreator = when(commandLineArguments.metadataOutputFormat) {
-                "json"                    -> JsonMetadataCreator()
-                in arrayOf("yaml", "yml") -> YamlMetadataCreator()
-                "txt"                     -> TextMetadataCreator()
-                else                      -> throw IllegalArgumentException("Metadata Output Format must be one of {yaml, json, txt}")
-            }
-
-            val metadata = metadataCreator.create(spriteBoundsList)
-            spriteSheetsWithMetadata.add(ProcessedSpriteSheet(packedSpriteSheet, metadata))
         }
+
+        logger.info("Finished unpacking spritesheets.")
     }
 }
