4ad5dc24368324ccbb2fffa049a4334af7ea26c3
Christian Broomfield <christian.broomfield@posteo.de>
2016-05-25 15:45:17 +0200

    Mod: Finished function declaration.

--- src/main/java/com/sbg/rpg/packer/SpriteSheetPacker.kt
+++ src/main/java/com/sbg/rpg/packer/SpriteSheetPacker.kt
@@ -6,7 +6,10 @@ import java.awt.image.BufferedImage
  * TODO: Write me.
  */
 class SpriteSheetPacker {
+    /**
+     * TODO: Write me.
+     */
     fun pack(sprites: List<BufferedImage>): BufferedImage {
-
+        throw NotImplementedError()
     }
 }
